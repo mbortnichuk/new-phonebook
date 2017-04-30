94a9a855b288e54744121c04d4d2533d6f702707
mbortnichuk <mariana.bortnichuk@gmail.com>
2017-04-30 13:53:49 +0300

    initial commit

---
+++ src/main/java/edu/mbortnichuk/newphonebook/Main.java
@@ -0,0 +1,10 @@
+package edu.mbortnichuk.newphonebook;
+
+/**
+ * Created by Mariana on 30-Apr-17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
